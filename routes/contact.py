73c039026870e8789c4b7e582d2ee2db5933fbaf
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-29 16:26:44 -0500

    :recycle: Constants are included in dependencies

--- routes/contact.py
+++ routes/contact.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from hubspot.crm.contacts.exceptions import ApiException
+from constants.constants import *
 from config.db import get_session
 from repositories.clickup import ClickUp
 from repositories.hubspot import Hubspot
@@ -12,15 +13,8 @@ from schemas.contact import ContactRequest
 contact = APIRouter(prefix='/contact')
 
 SessionDependency = Annotated[Session, Depends(get_session)]
-HubspotDependency = Annotated[Hubspot, Depends(
-    lambda : Hubspot('pat-na1-bfa3f0c0-426b-4f0e-b514-89b20832c96a')
-)]
-ClickupDependency = Annotated[ClickUp, Depends(
-    lambda : ClickUp(
-        'pk_3182376_Q233NZDZ8AVULEGGCHLKG2HFXWD6MJLC',
-        900200532843
-    )
-)]
+HubspotDependency = Annotated[Hubspot, Depends(lambda : Hubspot(HUBSPOT_TOKEN))]
+ClickupDependency = Annotated[ClickUp, Depends(lambda : ClickUp(CLICKUP_TOKEN, CLICKUP_LIST_ID))]
 
 @contact.post('')
 def add_contact(contact_request: ContactRequest, hubspot: HubspotDependency,  session: SessionDependency):
