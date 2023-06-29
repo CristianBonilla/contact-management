7cd60b9937e9779b76a2c1802c14ede57277714f
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-29 16:17:06 -0500

    :recycle: A hubspot class to include all the given functions

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from config.db import engine, Base
 from routes.contact import contact
 from middlewares.api_call import ApiCallMiddleware
-from repositories.hubspot import load_clickup_state_property
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,5 +10,3 @@ app = FastAPI()
 app.include_router(contact)
 
 app.add_middleware(ApiCallMiddleware)
-
-load_clickup_state_property()
--- repositories/hubspot.py
+++ repositories/hubspot.py
@@ -5,148 +5,152 @@ from hubspot.crm.contacts.exceptions import ApiException
 from hubspot.crm.properties import ModelProperty, PropertyCreate
 from schemas.contact import ContactRequest, ClickUpState, UpdateHubspotContacts
 
-client = hubspot.Client.create(access_token='pat-na1-bfa3f0c0-426b-4f0e-b514-89b20832c96a')
+class Hubspot:
+    def __init__(self, token: str):
+        self.client = hubspot.Client.create(access_token=token)
+        self.load_clickup_state_property()
 
-def add_contact(contact_request: ContactRequest, clickup_state = ClickUpState.NOT_SYNCHRONIZED):
-    contact_object = SimplePublicObjectInputForCreate(
-        properties={
-            'email': contact_request.email,
-            'firstname': contact_request.firstname,
-            'lastname': contact_request.lastname,
-            'phone': contact_request.phone,
-            'website': contact_request.website,
-            'clickup_state': clickup_state.value
-        },
-        associations=[]
-    )
-    try:
-        contact = client.crm.contacts.basic_api.create(
-            simple_public_object_input_for_create=contact_object
-        )
-        return {
-            'id': contact.id,
-            'properties': contact.properties,
-            'properties_with_history': contact.properties_with_history,
-            'archived': contact.archived,
-            'archived_at': contact.archived_at
-        }
-    except ApiException as exception:
-        raise exception
-
-def update_contacts(update_contacts: List[UpdateHubspotContacts], clickup_state = ClickUpState.SYNCHRONIZED):
-    update_contacts_object = BatchInputSimplePublicObjectBatchInput(
-        inputs=list(map(
-            lambda contact : {
-                'id': contact.id,
-                'properties': {
-                    'email': contact.properties.email,
-                    'firstname': contact.properties.firstname,
-                    'lastname': contact.properties.lastname,
-                    'phone': contact.properties.phone,
-                    'website': contact.properties.website,
-                    'clickup_state': clickup_state.value
-                }
-            }, update_contacts
-        )))
-    try:
-        contacts = client.crm.contacts.batch_api.update(
-            batch_input_simple_public_object_batch_input=update_contacts_object
-        )
-        return list(map(
-            lambda contact : {
-                'id': contact.id,
-                'properties': contact.properties,
-                'properties_with_history': contact.properties_with_history,
-                'archived': contact.archived,
-                'archived_at': contact.archived_at
-            }, contacts.results
-        ))
-    except ApiException as expression:
-        raise expression
-
-def search_by_contacts_clickup_state(clickup_state = ClickUpState.NOT_SYNCHRONIZED, limit=100):
-    contact_search = PublicObjectSearchRequest(
-        filter_groups=[
-            {
-                'filters': [
-                    {
-                        'propertyName': 'clickup_state',
-                        'value': clickup_state.value,
-                        'operator': 'EQ'
-                    }
-                ]
-            }
-        ],
-        properties=[
-            'email',
-            'firstname',
-            'lastname',
-            'phone',
-            'website',
-            'clickup_state'
-        ],
-        limit=limit)
-    try:
-        contacts = client.crm.contacts.search_api.do_search(
-            public_object_search_request=contact_search
-        )
-        return list(map(
-            lambda contact : {
-                'id': contact.id,
-                'properties': contact.properties,
-                'properties_with_history': contact.properties_with_history,
-                'archived': contact.archived,
-                'archived_at': contact.archived_at
-            }, contacts.results
-        ))
-    except ApiException as expression:
-        raise expression
-
-def load_clickup_state_property():
-    clickup_state_property: ModelProperty
-    try:
-        clickup_state_property = client.crm.properties.core_api.get_by_name(
-            object_type='contacts',
-            property_name='clickup_state',
-            archived=False
-        )
-        return clickup_state_property
-    except ApiException as exception:
-        if (exception.status == 404):
-            clickup_state_property = create_clickup_state_property()
-        raise exception
-
-def create_clickup_state_property():
-    property_object = PropertyCreate(
-        name='clickup_state',
-        label='ClickUp State',
-        type='bool',
-        field_type='booleancheckbox',
-        group_name='contactinformation',
-        options=[
-            {
-                'label': 'Synchronized',
-                'description': 'The contact was synced in ClickUp',
-                'value': True,
-                'displayOrder': 1,
-                'hidden': False
+    def add_contact(self, contact_request: ContactRequest, clickup_state = ClickUpState.NOT_SYNCHRONIZED):
+        contact_object = SimplePublicObjectInputForCreate(
+            properties={
+                'email': contact_request.email,
+                'firstname': contact_request.firstname,
+                'lastname': contact_request.lastname,
+                'phone': contact_request.phone,
+                'website': contact_request.website,
+                'clickup_state': clickup_state.value
             },
-            {
-                'label': 'Not Synchronized',
-                'description': 'The contact has not been synced in ClickUp',
-                'value': False,
-                'displayOrder': 2,
-                'hidden': False
-            }
-        ],
-        has_unique_value=False,
-        hidden=False,
-        form_field=True)
-    try:
-        clickup_state_property = client.crm.properties.core_api.create(
-            object_type='contacts',
-            property_create=property_object
+            associations=[]
         )
-        return clickup_state_property
-    except ApiException as exception:
-        raise exception
+        try:
+            contact = self.client.crm.contacts.basic_api.create(
+                simple_public_object_input_for_create=contact_object
+            )
+            return {
+                'id': contact.id,
+                'properties': contact.properties,
+                'properties_with_history': contact.properties_with_history,
+                'archived': contact.archived,
+                'archived_at': contact.archived_at
+            }
+        except ApiException as exception:
+            raise exception
+
+    def update_contacts(self, update_contacts: List[UpdateHubspotContacts], clickup_state = ClickUpState.SYNCHRONIZED):
+        update_contacts_object = BatchInputSimplePublicObjectBatchInput(
+            inputs=list(map(
+                lambda contact : {
+                    'id': contact.id,
+                    'properties': {
+                        'email': contact.properties.email,
+                        'firstname': contact.properties.firstname,
+                        'lastname': contact.properties.lastname,
+                        'phone': contact.properties.phone,
+                        'website': contact.properties.website,
+                        'clickup_state': clickup_state.value
+                    }
+                }, update_contacts
+            )))
+        try:
+            contacts = self.client.crm.contacts.batch_api.update(
+                batch_input_simple_public_object_batch_input=update_contacts_object
+            )
+            return list(map(
+                lambda contact : {
+                    'id': contact.id,
+                    'properties': contact.properties,
+                    'properties_with_history': contact.properties_with_history,
+                    'archived': contact.archived,
+                    'archived_at': contact.archived_at
+                }, contacts.results
+            ))
+        except ApiException as expression:
+            raise expression
+
+    def search_by_contacts_clickup_state(self, clickup_state = ClickUpState.NOT_SYNCHRONIZED, limit=100):
+        contact_search = PublicObjectSearchRequest(
+            filter_groups=[
+                {
+                    'filters': [
+                        {
+                            'propertyName': 'clickup_state',
+                            'value': clickup_state.value,
+                            'operator': 'EQ'
+                        }
+                    ]
+                }
+            ],
+            properties=[
+                'email',
+                'firstname',
+                'lastname',
+                'phone',
+                'website',
+                'clickup_state'
+            ],
+            limit=limit)
+        try:
+            contacts = self.client.crm.contacts.search_api.do_search(
+                public_object_search_request=contact_search
+            )
+            return list(map(
+                lambda contact : {
+                    'id': contact.id,
+                    'properties': contact.properties,
+                    'properties_with_history': contact.properties_with_history,
+                    'archived': contact.archived,
+                    'archived_at': contact.archived_at
+                }, contacts.results
+            ))
+        except ApiException as expression:
+            raise expression
+
+    def load_clickup_state_property(self):
+        clickup_state_property: ModelProperty
+        try:
+            clickup_state_property = self.client.crm.properties.core_api.get_by_name(
+                object_type='contacts',
+                property_name='clickup_state',
+                archived=False
+            )
+            print(clickup_state_property)
+            return clickup_state_property
+        except ApiException as exception:
+            if (exception.status == 404):
+                clickup_state_property = self.create_clickup_state_property()
+            raise exception
+
+    def create_clickup_state_property(self):
+        property_object = PropertyCreate(
+            name='clickup_state',
+            label='ClickUp State',
+            type='bool',
+            field_type='booleancheckbox',
+            group_name='contactinformation',
+            options=[
+                {
+                    'label': 'Synchronized',
+                    'description': 'The contact was synced in ClickUp',
+                    'value': True,
+                    'displayOrder': 1,
+                    'hidden': False
+                },
+                {
+                    'label': 'Not Synchronized',
+                    'description': 'The contact has not been synced in ClickUp',
+                    'value': False,
+                    'displayOrder': 2,
+                    'hidden': False
+                }
+            ],
+            has_unique_value=False,
+            hidden=False,
+            form_field=True)
+        try:
+            clickup_state_property = self.client.crm.properties.core_api.create(
+                object_type='contacts',
+                property_create=property_object
+            )
+            return clickup_state_property
+        except ApiException as exception:
+            raise exception
