41d493c94d88d9f4e5b53249f515db7d9f165a26
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-29 02:16:24 -0500

    :construction: An endpoint is built to save contact in hubspot

--- routes/contact.py
+++ routes/contact.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter
+import json
+from fastapi import APIRouter, HTTPException
+from hubspot.crm.contacts.exceptions import ApiException
+import repositories.hubspot as hubspot_repo
+from schemas.contact import ContactRequest
 
 contact = APIRouter(prefix='/contact')
 
-@contact.post('/')
-def add():
-    return
+@contact.post('')
+def add_contact(contact_request: ContactRequest):
+    try:
+        hubspot_repo.load_clickup_state_property()
+        return hubspot_repo.add_contact(contact_request)
+    except ApiException as exception:
+        raise HTTPException(
+            status_code=exception.status,
+            detail={ 'errors': [json.loads(exception.body)] }
+        )
 
-@contact.post('/sync')
-def sync():
+@contact.post('/synchronized')
+def sync_contacts():
     return
