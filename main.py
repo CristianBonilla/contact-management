b03ad43d449bb1b7c66005df7a0babc5cae57976
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-30 14:40:09 -0500

    :fire: api_call middleware is overridden on startup

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 from config.db import engine, Base
 from routes.contact import contact
-from middlewares.api_call import ApiCallMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(contact)
-
-app.add_middleware(ApiCallMiddleware)
