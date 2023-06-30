31d54252edd9573c82d5fcab9acc371fd7b97a88
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-30 14:59:29 -0500

    :boom: Updated contact routes with background tasks included

--- routes/contact.py
+++ routes/contact.py
@@ -1,10 +1,12 @@
 import json
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from hubspot.crm.contacts.exceptions import ApiException
 from constants.constants import *
 from config.db import get_session
+from tasks.sync import run_contacts_sync
 from repositories.clickup import ClickUp
 from repositories.hubspot import Hubspot
 import repositories.contact as contact_repo
@@ -17,7 +19,7 @@ HubspotDependency = Annotated[Hubspot, Depends(lambda : Hubspot(HUBSPOT_TOKEN))]
 ClickupDependency = Annotated[ClickUp, Depends(lambda : ClickUp(CLICKUP_TOKEN, CLICKUP_LIST_ID))]
 
 @contact.post('')
-def add_contact(contact_request: ContactRequest, hubspot: HubspotDependency,  session: SessionDependency):
+async def add_contact(contact_request: ContactRequest, hubspot: HubspotDependency,  session: SessionDependency):
     try:
         hubspot_contact = hubspot.add_contact(contact_request)
         contact_repo.add_contact(session, contact_request)
@@ -29,6 +31,14 @@ def add_contact(contact_request: ContactRequest, hubspot: HubspotDependency,  se
         )
 
 @contact.post('/sync')
-def sync_contacts(hubspot: HubspotDependency, clickup: ClickupDependency):
+async def sync_contacts(background_tasks: BackgroundTasks, hubspot: HubspotDependency, clickup: ClickupDependency):
     contacts = hubspot.search_by_contacts_clickup_state()
-    return contacts
+    background_tasks.add_task(run_contacts_sync, hubspot, clickup, contacts)
+    return JSONResponse(
+        status_code=200,
+        content={
+            'total': len(contacts),
+            'description': 'Following contacts will be synced in ClickUp with background tasks',
+            'contacts': contacts
+        }
+    )
