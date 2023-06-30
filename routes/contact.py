4937b6b2cca2327aadc8a95498dff146d44ce661
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-30 16:03:01 -0500

    :hand: Events that occurred for each request are recorded

--- routes/contact.py
+++ routes/contact.py
@@ -1,15 +1,17 @@
 import json
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
+from fastapi import APIRouter, Depends, HTTPException, Request, BackgroundTasks
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from hubspot.crm.contacts.exceptions import ApiException
 from constants.constants import *
 from config.db import get_session
 from tasks.sync import run_contacts_sync
+import repositories.api_call as api_call_repo
 from repositories.clickup import ClickUp
 from repositories.hubspot import Hubspot
 import repositories.contact as contact_repo
+from schemas.api_call import ApiCallRequest
 from schemas.contact import ContactRequest
 
 contact = APIRouter(prefix='/contact')
@@ -19,26 +21,54 @@ HubspotDependency = Annotated[Hubspot, Depends(lambda : Hubspot(HUBSPOT_TOKEN))]
 ClickupDependency = Annotated[ClickUp, Depends(lambda : ClickUp(CLICKUP_TOKEN, CLICKUP_LIST_ID))]
 
 @contact.post('')
-async def add_contact(contact_request: ContactRequest, hubspot: HubspotDependency,  session: SessionDependency):
+async def add_contact(contact_request: ContactRequest, hubspot: HubspotDependency, session: SessionDependency):
+    endpoint = contact.url_path_for('add_contact')
+    params = contact_request
     try:
         hubspot_contact = hubspot.add_contact(contact_request)
         contact_repo.add_contact(session, contact_request)
+        api_call_repo.add_api_call(session, ApiCallRequest(
+            endpoint=endpoint,
+            params=params,
+            result=json.dumps(hubspot_contact)
+        ))
         return hubspot_contact
     except ApiException as exception:
+        detail = { 'errors': [json.loads(exception.body)] }
+        api_call_repo.add_api_call(session, ApiCallRequest(
+            endpoint=endpoint,
+            params=params,
+            result=json.dumps({ 'detail': detail })
+        ))
         raise HTTPException(
             status_code=exception.status,
-            detail={ 'errors': [json.loads(exception.body)] }
+            detail=detail
         )
 
 @contact.post('/sync')
-async def sync_contacts(background_tasks: BackgroundTasks, hubspot: HubspotDependency, clickup: ClickupDependency):
+async def sync_contacts(request: Request, background_tasks: BackgroundTasks, hubspot: HubspotDependency, clickup: ClickupDependency, session: SessionDependency):
+    endpoint = contact.url_path_for('sync_contacts')
+    params = await request.json()
     contacts = hubspot.search_by_contacts_clickup_state()
-    background_tasks.add_task(run_contacts_sync, hubspot, clickup, contacts)
+    api_call_repo.add_api_call(session, ApiCallRequest(
+        endpoint=endpoint,
+        params=params,
+        result=json.dumps(contacts)
+    ))
+    background_tasks.add_task(
+        run_contacts_sync,
+        endpoint,
+        params,
+        hubspot,
+        clickup,
+        session,
+        contacts)
+    content = {
+        'total': len(contacts),
+        'description': 'Following contacts will be synced in ClickUp with background tasks',
+        'contacts': contacts
+    }
     return JSONResponse(
         status_code=200,
-        content={
-            'total': len(contacts),
-            'description': 'Following contacts will be synced in ClickUp with background tasks',
-            'contacts': contacts
-        }
+        content=content
     )
--- tasks/sync.py
+++ tasks/sync.py
@@ -1,12 +1,27 @@
-from typing import List, Dict, Any
+import json
 import asyncio
+from typing import List, Dict, Any
 from constants.constants import *
 from starlette.concurrency import run_in_threadpool
+from sqlalchemy.orm import Session
 from repositories.clickup import ClickUp
 from repositories.hubspot import Hubspot
+import repositories.api_call as api_call_repo
+from schemas.api_call import ApiCallRequest
 from schemas.contact import ContactBase, HubspotContact
 
-def hubspot_contacts_to_clickup(hubspot: Hubspot, clickup: ClickUp, data_list: List[Dict[str, Any]]):
+def hubspot_update_contacts(endpoint: str, params, hubspot: Hubspot, session: Session, contacts: List[HubspotContact]):
+    print('Updating contacts state in Hubspot...')
+    updated_contacts = hubspot.update_contacts(update_contacts=contacts)
+    api_call_repo.add_api_call(session, ApiCallRequest(
+        endpoint=endpoint,
+        params=params,
+        result=json.dumps(updated_contacts)
+    ))
+    print(updated_contacts)
+    print('Updated contacts state')
+
+def hubspot_contacts_to_clickup(endpoint: str, params, hubspot: Hubspot, clickup: ClickUp, session: Session, data_list: List[Dict[str, Any]]):
     if len(data_list) == 0:
         print('No contacts to sync')
         return
@@ -25,12 +40,14 @@ def hubspot_contacts_to_clickup(hubspot: Hubspot, clickup: ClickUp, data_list: L
     ))
     synced_tasks = []
     asyncio.run(clickup.create_tasks(contacts, synced_tasks))
+    api_call_repo.add_api_call(session, ApiCallRequest(
+        endpoint=endpoint,
+        params=params,
+        result=json.dumps(synced_tasks)
+    ))
     print(synced_tasks)
     print('Full synchronization')
-    print('Updating contacts state in Hubspot...')
-    updated_contacts = hubspot.update_contacts(update_contacts=contacts)
-    print(updated_contacts)
-    print('Updated contacts state')
+    hubspot_update_contacts(endpoint, params, hubspot, session, contacts)
 
-async def run_contacts_sync(hubspot: Hubspot, clickup: ClickUp, data_list: List[Dict[str, Any]]):
-    await run_in_threadpool(lambda : hubspot_contacts_to_clickup(hubspot, clickup, data_list))
+async def run_contacts_sync(endpoint: str, params, hubspot: Hubspot, clickup: ClickUp, session: Session, data_list: List[Dict[str, Any]]):
+    await run_in_threadpool(lambda : hubspot_contacts_to_clickup(endpoint, params, hubspot, clickup, session, data_list))
